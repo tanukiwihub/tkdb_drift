612b1518a7c955a923dc46b30e113fa6671abe0c
miikaeru <28813834+miikaeru@users.noreply.github.com>
2023-03-01 19:43:01 +0900

    Initial commit

---
+++ bin/tkdb_drift.dart
@@ -0,0 +1,5 @@
+import 'package:tkdb_drift/tkdb_drift.dart' as tkdb_drift;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tkdb_drift.calculate()}!');
+}
---
+++ test/tkdb_drift_test.dart
@@ -0,0 +1,8 @@
+import 'package:tkdb_drift/tkdb_drift.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
